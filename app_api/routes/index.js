b306110140c9d7946ceb043c4493cf76eac61feb
gingersnap21 <sawright2@buffs.wtamu.edu>
2017-11-12 20:17:52 +0000

    updated/fixed climbData

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,58 +1,22 @@
-var mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
+var ctrlAirplaneData = require('../controllers/airplane');
+var ctrlAirportData = require('../controllers/airport');
 
-var connectionstring = "mongodb://airplaneData:alohomora@ds113775.mlab.com:13775/airplane-data";
-mongoose.connect(connectionstring, { useMongoClient: true });
-
-mongoose.Promise = global.Promise;
-
-var climbDataSchema = new mongoose.Schema({
-    weight: Number,
-    vfri5: Number,
-    vfri10: Number,
-    vfri15: Number,
-    vClmb: Number
-});
-
-var ClimbData = mongoose.model('ClimbData', climbDataSchema, 'ClimbData');
-
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  
-  //console.log("I am here");
-  
-  ClimbData.find({}, function(err, climbdatas){
-    if(err){
-        res.send(err);
-        return console.error(err)
-        
-            
-        var output = "";
-        
-        climbdatas.forEach(function(climbdata) {
-            console.log(climbdata.weight);
-            output += "weight: " + climbdata.weight + "<br/>";
-        })
-    }
-    
-/*   
-    for(var i = 0; i < climbdatas.length; i++){
-        console.log(climbdatas[i]);
-        output += climbdatas[i].weight + "<br/>";
-    }
-*/
-    res.send(output);
-    res.render('airplanes', {title: 'Airplanes Project', outputs: output})
-  })
-/*  
-    Kitten.find(function (err, kittens) {
-    if (err) return console.error(err);
-    console.log(kittens);
-    })
-  
-  res.send('JackPot!'); */
+  res.render('index', { title: 'Express' });
 });
+*/
+
+/* CLIMB DATA */
+router.get('/climbData/:weight', ctrlAirplaneData.climbDataReadOne);
+router.get('/climbData', ctrlAirplaneData.climbDataReadAll);
+
+/* LANDING DATA */
+router.get('/landingData/:weight', ctrlAirplaneData.landingDataReadOne);
+
+/* AIRPORT DATA */
+router.get('/airportData', ctrlAirportData.airportDataReadAll);
 
 module.exports = router;
